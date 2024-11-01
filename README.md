36087615290b16ea4d6ed79e7d3cba2226f170cb
rouelboom <git@stackblitz.com>
2024-11-01 14:44:59 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# warhammer_40k_ladder
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/rouelboom/warhammer_40k_ladder)
---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export interface Player {
+  id: string;
+  name: string;
+  faction: string;
+  points: number;
+  wins: number;
+  losses: number;
+}
+
+export interface Game {
+  id: string;
+  date: string;
+  player1: string;
+  player2?: string;
+  status: 'open' | 'matched' | 'completed';
+  points: number;
+  winner?: string;
+  faction1?: string;
+  faction2?: string;
+}</content>
